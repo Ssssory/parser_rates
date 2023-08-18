73875c0a5b5f078b82b72b45d368ff79619065e3
trubiv <itrubchaninov@list.ru>
2023-08-18 05:41:23 +0300

    add check date

--- src/app/Http/Controllers/RateController.php
+++ src/app/Http/Controllers/RateController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RateRequest;
+use App\Models\Rate;
 use App\Services\RateService;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 
 class RateController extends Controller
@@ -18,6 +20,13 @@ class RateController extends Controller
     {
 
         $data = $request->data();
+        $targetDay = Carbon::parse($data->date);
+        if (!Rate::where('history_date', $targetDay)->exists()) {
+            return new JsonResponse([
+                'status' => 'error',
+                'message' => 'No data for this date'
+            ], JsonResponse::HTTP_NOT_FOUND);
+        }
 
         $rate = $this->rateService->getRate($data);
 
